5abefdfd6ab355f018e502b2fae847c2513124ba
hayongwoon <a01046641857@gmail.com>
2022-06-29 17:24:12 +0900

    fix drf setting

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import Group
 from django.contrib import admin
-
+from .models import User, UserProfile, WatchList
 # Register your models here.
+
+# unregister group
+admin.site.unregister(Group)
+
+class Useradmin(admin.ModelAdmin):
+    list_display = ('id', 'phone', 'username' )
+
+
+admin.site.register(User, Useradmin)
+admin.site.register(UserProfile)
+admin.site.register(WatchList)
--- user/views.py
+++ user/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#회원 가입
+#로그인
+#로그아웃
+#마이페이지
+#계정 비활성화
+#로그인 한 사용자의 구매 목록
+#로그인 한 사용자의 판매 목록
