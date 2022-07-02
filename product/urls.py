797b430a576df876e8a863ce32cf285d59458874
hayongwoon <a01046641857@gmail.com>
2022-07-02 16:03:22 +0900

    사용자 별 판매 목록 뷰

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from product.views import ProductApiView, SingleProductApiView
+from product.views import ProductApiView, SingleProductApiView, SellingListApiView
 
 
 urlpatterns = [
     path('', ProductApiView.as_view()),
+    
+    # 사용자 판매 목록 뷰
+    path('<user_id>/sellinglist/', SellingListApiView.as_view()),
+
+    # 단일 상품 뷰
     path('<obj_id>/', SingleProductApiView.as_view()),
+    
 
 ]
--- product/views.py
+++ product/views.py
@@ -1,38 +1,17 @@
-from functools import partial
-from multiprocessing import context
 from django.shortcuts import render
-import datetime
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import IsRegisterdMoreThanTwoRliabilityPoint
 
 from product.serializers import ProductSerializer
 from product.models import Product as ProductModel
 
 
-class IsRegisterdMoreThanTwoRliabilityPoint(BasePermission):
-    """
-    거래 신뢰도가 2 보다 높은 사용자는 쓰기, 수정, 삭제 가능
-    이외에는 조회만 가능
-    """
-    SAFE_METHODS = ('GET', )
-    message = '접근 권한이 없습니다.'
-
-    def has_permission(self, request, view):
-        user = request.user
-
-        if request.method in self.SAFE_METHODS or \
-            user.is_authenticated and request.user.deal_reliability_avg > 2:
-
-            return True
-
-        return False
-
 # Create your views here.
 class ProductApiView(APIView):
-    permission_classes = [IsRegisterdMoreThanTwoRliabilityPoint]
+    # permission_classes = [IsRegisterdMoreThanTwoRliabilityPoint]
     # 상품 등록
     def post(self, request):
         serializer = ProductSerializer(data=request.data, context={'request': request}, partial=True)
@@ -43,12 +22,12 @@ class ProductApiView(APIView):
 
     # 모든 상품 조회(등록 순, 팔고있는is_active=True)
     def get(self, request):
-        products = ProductModel.objects.filter(is_active=True).order_by('-register_date')
-        return Response(ProductSerializer(products, many=True).data, status=status.HTTP_200_OK)
+        active_products = ProductModel.objects.filter(is_active=True).order_by('-register_date')
+        return Response(ProductSerializer(active_products, many=True).data, status=status.HTTP_200_OK)
 
 
 class SingleProductApiView(APIView):
-    permission_classes = [IsRegisterdMoreThanTwoRliabilityPoint]
+    # permission_classes = [IsRegisterdMoreThanTwoRliabilityPoint]
     # 단일 상품 조회
     def get(self, request, obj_id):
         product = ProductModel.objects.get(id=obj_id)
@@ -71,4 +50,12 @@ class SingleProductApiView(APIView):
         except:
             return Response({"message":"이미 삭제 된 상품입니다."})
 
-# 사용자의 판매 목록
+
+class SellingListApiView(APIView):
+    # 사용자의 판매 목록
+    def get(self, request, user_id):
+        user_products = ProductModel.objects.filter(user=user_id).order_by('-register_date') 
+        return Response(ProductSerializer(user_products, many=True).data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        return Response({'msg':'post good'})
