881657f39cc6db8b6914013d4079d3e4f23e69e8
hayongwoon <a01046641857@gmail.com>
2022-07-02 17:18:33 +0900

    product  url 수정

--- product/urls.py
+++ product/urls.py
@@ -4,6 +4,7 @@ from product.views import ProductApiView, SingleProductApiView, SellingListApiVi
 
 
 urlpatterns = [
+    # 상품 등록 및 모든 상품 조회(최신순) 뷰
     path('', ProductApiView.as_view()),
     
     # 사용자 판매 목록 뷰
