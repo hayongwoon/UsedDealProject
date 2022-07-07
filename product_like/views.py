f3d0c9b2bd711b0d8ad68be8252bfa3610c586fb
hayongwoon <a01046641857@gmail.com>
2022-07-08 03:50:09 +0900

    좋아요 기능 리펙터링

--- product_like/views.py
+++ product_like/views.py
@@ -11,14 +11,14 @@ from product.models import Product as ProductModel
 class LikeApiView(APIView):
     # 좋아요 생성 
     def post(self, request, product_id):
-        LikeModel.objects.create(user_id=16, product_id=product_id)
+        LikeModel.objects.create(user_id=request.user.id, product_id=product_id)
         ProductModel.objects.filter(id=product_id).update(like_cnt=F("like_cnt") + 1)
 
         return Response({"msg":"좋아요"})
 
     # 좋아요 취소
     def delete(self, request, product_id):
-        delete_cnt, _ = LikeModel.objects.filter(user_id=16, product_id=product_id).delete()
+        delete_cnt, _ = LikeModel.objects.filter(user_id=request.user.id, product_id=product_id).delete()
         if delete_cnt: # 좋아요 수가 0보다 클 때
             ProductModel.objects.filter(id=product_id).update(like_cnt=F("like_cnt") - 1)
             return Response({"msg":"좋아요 취소"})
